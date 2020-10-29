5632084c27be3350d611db805ab7f5620b40c506
Cleomir <cleomir.mg@hotmail.com>
2020-10-29 18:57:01 +0200

    Add router

--- hello-world-app/src/main.js
+++ hello-world-app/src/main.js
@@ -1,8 +1,27 @@
 import Vue from "vue";
+import VueRouter from "vue-router";
 import App from "./App.vue";
+import Nested from "./components/Nested.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: Nested,
+  },
+  {
+    path: "/hello",
+    component: HelloWorld,
+  },
+];
+const router = new VueRouter({
+  routes,
+});
 
 new Vue({
   render: (h) => h(App),
+  router,
 }).$mount("#app");
