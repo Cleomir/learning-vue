fa870b913876e51ec6232cf97ec24f3df5c10efe
Cleomir <cleomir.mg@hotmail.com>
2020-11-04 09:49:25 +0200

    Add form binding

--- hello-world-app/src/main.js
+++ hello-world-app/src/main.js
@@ -3,7 +3,8 @@ import VueRouter from "vue-router";
 import App from "./App.vue";
 import Nested from "./components/Nested.vue";
 import HelloWorld from "./components/HelloWorld.vue";
-import LifecycleHooks from './components/LifecycleHooks.vue';
+import LifecycleHooks from "./components/LifecycleHooks.vue";
+import FormBinding from "./components/FormBinding.vue";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
@@ -20,8 +21,12 @@ const routes = [
   },
   {
     path: "/lifecycle",
-    component: LifecycleHooks
-  }
+    component: LifecycleHooks,
+  },
+  {
+    path: "/form-binding",
+    component: FormBinding,
+  },
 ];
 const router = new VueRouter({
   routes,
