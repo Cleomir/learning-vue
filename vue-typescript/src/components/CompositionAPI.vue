773a0a6764d1391e1f1deb0bad5c164f80d3244d
Cleomir <cleomir.mg@hotmail.com>
2021-01-07 13:58:22 +0200

    Add reactive, watch and watchEffect uses

--- vue-typescript/src/components/CompositionAPI.vue
+++ vue-typescript/src/components/CompositionAPI.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <p>Hello, {{ name }}!</p>
+    <p>Person: {{ person.name }} {{ person.age }}</p>
     <button @click="changeName">Change name</button>
+    <button @click="changePerson">Change person</button>
     <div>
       <span>{{ nameToUpperCase }}</span>
     </div>
@@ -9,21 +11,46 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from "vue";
+import {
+  defineComponent,
+  ref,
+  computed,
+  reactive,
+  watch,
+  watchEffect,
+} from "vue";
 
 export default defineComponent({
   setup() {
     const name = ref("John");
+    const person = reactive({ name: "Jane", age: 30 });
+
     const changeName = () => {
       name.value = "Doe";
     };
+
+    const changePerson = () => {
+      person.name = "Daisy";
+      person.age = 18;
+    };
+
     const nameToUpperCase = computed(() => {
       return name.value.toUpperCase();
     });
 
+    watch(person, () => {
+      console.log("Person has changed!");
+    });
+
+    watchEffect(() => {
+      console.log("Watch effect! ", name);
+    });
+
     return {
       name,
+      person,
       changeName,
+      changePerson,
       nameToUpperCase,
     };
   },
