816e0f71c24bdec204faafea4b6cf0f87a0344a9
Cleomir <cleomir.mg@hotmail.com>
2021-01-09 13:38:23 +0200

    Add lifecycle hooks

--- vue-typescript/src/components/CompositionAPI.vue
+++ vue-typescript/src/components/CompositionAPI.vue
@@ -19,6 +19,8 @@ import {
   reactive,
   watch,
   watchEffect,
+  onMounted,
+  onUnmounted,
 } from "vue";
 
 import Props from "./Props.vue";
@@ -26,30 +28,34 @@ import Props from "./Props.vue";
 export default defineComponent({
   components: { Props },
   setup() {
+    // data
     const name = ref("John");
     const person = reactive({ name: "Jane", age: 30 });
     const props = ref([{ name: "Prop1" }, { name: "Prop2" }]);
 
+    // methods
     const changeName = () => {
       name.value = "Doe";
     };
-
     const changePerson = () => {
       person.name = "Daisy";
       person.age = 18;
     };
-
     const nameToUpperCase = computed(() => {
       return name.value.toUpperCase();
     });
 
+    // watchers
     watch(person, () => {
       console.log("Person has changed!");
     });
-
     watchEffect(() => {
       console.log("Watch effect! ", name);
     });
+
+    //lifecycle hooks
+    onMounted(() => console.log("component mounted"));
+    onUnmounted(() => console.log("component unmounted"));
 
     return {
       name,
